bb6d20900e7393f89b61cda521f2e3e9147b87c8
han <qwertpiyi@gmail.com>
2021-10-12 01:00:52 +0800

    stash

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import ChatList from "./page/chat/ChatList.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+    <ChatList />
+  </div>
 </template>
 
 <style>
---
+++ src/page/chat/ChatList.vue
@@ -0,0 +1,21 @@
+<script  lang='ts'>
+export default {
+  data() {
+    return {
+      count: 2,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="box">dddd</div>
+  <div>{{ count }}</div>
+  <div @click="log">{{ msg }}</div>
+</template>
+
+<style scoped>
+.box {
+  color: red;
+}
+</style>
