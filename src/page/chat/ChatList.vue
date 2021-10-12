f0c121b8a7d637a0e0002d71b797ad0d6490e419
han <qwertpiyi@gmail.com>
2021-10-13 00:26:53 +0800

    stash

--- src/page/chat/ChatList.vue
+++ src/page/chat/ChatList.vue
@@ -1,17 +1,33 @@
+<script  lang='ts' setup>
+import { computed } from '@vue/reactivity';
+  console.log('setup');
+</script>
 <script  lang='ts'>
+console.log('common');
 export default {
-  data() {
+  data: () => {
     return {
       count: 2,
+      msg: 'ss'
     };
   },
+
+  methods: {
+    handleClick: () => {
+      console.log('handleClcik');
+    }
+  },
+
+  // computed: () => {
+  //   console.log('computed');
+  // }
 };
 </script>
 
 <template>
   <div class="box">dddd</div>
   <div>{{ count }}</div>
-  <div @click="log">{{ msg }}</div>
+  <div @click="handleClick">{{ msg }}</div>
 </template>
 
 <style scoped>
